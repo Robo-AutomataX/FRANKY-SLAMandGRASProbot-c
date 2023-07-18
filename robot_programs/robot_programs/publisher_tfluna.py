d11658e9c2d1403fbad7256b5915a05c431d2ba0
Brayan Martinez <brayanelgamesa@hotmail.com>
2023-07-17 19:29:09 -0600

    Se modifico el archivo de publisher_tfluna

--- robot_programs/robot_programs/publisher_tfluna.py
+++ robot_programs/robot_programs/publisher_tfluna.py
@@ -11,12 +11,12 @@ import time
 import math
 
 class MinimalPublisher(Node):
-    interface_luna = serial.Serial('/dev/serial0',115200)
+    interface_luna = serial.Serial('/dev/serial0',115200) 
 
     def __init__(self,frame_id):
         super().__init__('minimal_publisher')
 
-        self.ser_motores = serial.Serial('/dev/ttyACM1',115200)
+        self.ser_motores = serial.Serial('/dev/ttyACM1',115200) #It stablish the connection with the tfluna lidar in the raspberry and the computer
         self.publisher_ = self.create_publisher(LaserScan, 'lidar_publisher', 10)
         timer_period = 0.001  # seconds
         luna_enableOutput  = [0x5a,0x05,0x07,0x01,0x00]
@@ -24,28 +24,7 @@ class MinimalPublisher(Node):
         self.scan = LaserScan()
         self.contador = 1
         self.aux = 0
-        #self.scan.header.frame_id = 'map'
-        #self.scan.range_min = 0.0 #Distancia mínima que puede medir
-        #self.scan.range_max = 8.0 #Distancia máxima que puede medir
-        #self.scan.angle_max = math.pi #Ángulo máximo (media vuelta)
-        #self.scan.angle_min = 0.0 #Ángulo mínimo (cero grados)
-        #self.scan.angle_increment = math.pi/1024.0 #El angulo que barre en 1024 pasos
-
-        #self.scan.scan_time = 10.288484  #Tiempo total del escaneo
-        #self.scan.ranges = self.distances
-        #self.publisher_publisher(self.scan)
-
-        self.distances = []
-
-        #self.scan.angle_min = -1.57  # -90 grados
-        #self.scan.angle_max = 1.57  # 90 grados
-        #self.scan.angle_increment = 0.01  # Incremento de ángulo pequeño para mayor resolución
-        #self.scan.time_increment = 0.0  # No se necesita incremento de tiempo en este caso
-        #self.scan.scan_time = 0.1  # Tiempo total de escaneo
-        #self.scan.range_min = 0.0  # Distancia mínima de detección
-        #self.scan.range_max = 10.0  # Dis
-
-
+        self.distances = [] 
 
         if self.interface_luna.isOpen() :
             self.get_logger().info("TF-Luna sucessfully opened")
@@ -80,32 +59,18 @@ class MinimalPublisher(Node):
 
 
 
-                #self.scan.header.frame_id = 'map'
-                #self.scan.angle_min = 0.0
-                #self.scan.angle_max = math.pi
-                #self.scan.angle_increment = math.pi /4
-                #self.scan.range_min = 0.0
-                #self.scan.range_max = 7.0
-                #self.scan.time_increment = 1.0  # No se necesita incremento de tiempo en este c>
-                #self.scan.scan_time = 4.0  # Tiempo total de escaneo
-
-                #self.scan.ranges = [4.0,4.0,4.0,4.0]
-
-                #self.publisher_.publish(self.scan)
-
-
 
 
 
                 if self.ser_motores.in_waiting > 0:
                     serial_variable = self.ser_motores.readline().decode('utf-8')
 
-                    distance = float(distance)/100.0
+                    distance = float(distance)/100.0 #To convert the distance in cm to m
 
                     print("Paso: "+ serial_variable +" distancia: " +  str(distance))
-                    self.distances.append(distance)
-                    if len(self.distances)==1024:
+                    self.distances.append(distance) #Saves the distance in the array distances
 
+                    if len(self.distances)==1024:
                         if self.aux == 1:
                            self.distances.reverse()
                            self.aux = 0
@@ -114,18 +79,20 @@ class MinimalPublisher(Node):
 
 
                         self.scan.header.frame_id = 'map'
-                        self.scan.range_min = 0.0 #Distancia mínima que puede medir
-                        self.scan.range_max = 8.0 #Distancia máxima que puede medir
-                        self.scan.angle_max = math.pi #Ángulo máximo (media vuelta)
-                        self.scan.angle_min = 0.0 #Ángulo mínimo (cero grados)
-                        self.scan.angle_increment = math.pi/1024.0 #El angulo que barre en 1024 pasos
+                        self.scan.range_min = 0.0 #minimum distance of detection of the tfluna-lidar 0 m
+                        self.scan.range_max = 8.0 #maximum distance of detection of the tfluna-lidar 8.0 m
+                        self.scan.angle_max = math.pi #maximum angle 180 degrees
+                        self.scan.angle_min = 0.0 #minimum angle 0 degrees
+                        self.scan.angle_increment = math.pi/1024.0 #angle increment (in this case #maximum_angle/#steps)
 
-                        self.scan.scan_time = 10.288484  #Tiempo total del escaneo
-                        self.scan.ranges = self.distances
-                        self.publisher_.publish(self.scan)
+                        self.scan.scan_time = 10.288484  #total time in scanning (it was measure with the arduino device)
+                        self.scan.ranges = self.distances 
+                        self.publisher_.publish(self.scan) #publish the data in the node
+                        
+                        #self.scan.time_increment = 0.0  # increment of time (in this case is not necessary)
 
 
-                        print(self.distances)
+                        #print(self.distances)
                         self.distances = []
 
 
